5a78f14abe0f3b29eea1ddafe58d265e468a6a13
Yslas <jryslas@gmail.com>
2018-11-07 15:08:11 -0800

    needs node modules to gitignore

---
+++ data/friends.js
@@ -0,0 +1,27 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var userData = [
+    {
+      Name: "Jose Martinez",
+      Image: "Face",
+      Question1: "1",
+      Question2: "1",
+      Question3: "1",
+      Question4: "1",
+      Question5: "1",
+      Question6: "1",
+      Question7: "1",
+      Question8: "1",
+      Question9: "1",
+      Question10: "1",
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = userData;
+  
---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var app = express();
+// var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function(){
+console.log("App listening on PORT: " + PORT);
+});
+
+
